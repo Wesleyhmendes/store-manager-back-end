6ad7b79870577811fc669616c8d320211fdcbf4e
Wesley Mendes <wesleymendes123321@gmail.com>
2023-11-23 17:38:42 -0300

    Testes Service prontos

--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 const { productsService } = require('../../../src/services');
 const { productsModel } = require('../../../src/models');
 const { productsController } = require('../../../src/controllers');
-const { allProductsModel, producByIdModel } = require('../../mocks/products.mock');
+const { allProductsModel, producByIdModel, newProduct } = require('../../mocks/products.mock');
 
 describe('Realizando testes para listagem dos produtos no service', function () {
   it('Listando todos os produtos com sucesso no service', async function () {
@@ -39,6 +39,80 @@ describe('Realizando testes para listagem dos produtos no service', function () 
 
     expect(res.status.calledWith(404));
     expect(res.json.calledWith({ message: 'Product not found' }));
+  });
+
+  it('Cadastra produtos no service', async function () {
+    sinon.stub(productsModel, 'insertProductsModel').resolves(newProduct);
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.insertProductsService(newProduct);
+
+    expect(res.status.calledWith(404));
+    expect(res.json.calledWith({ message: 'Product not found' }));
+  });
+
+  it('Atualiza produtos no service', async function () {
+    const updateProductModelResolve = {
+      id: 1,
+      name: 'Martelo do Batman',
+    };
+    sinon.stub(productsModel, 'updateProductModel').resolves({ status: 200, data: updateProductModelResolve });
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.updateProductService(updateProductModelResolve);
+
+    expect(res.status.calledWith(200));
+    expect(res.json.calledWith({ status: 200, data: updateProductModelResolve }));
+  });
+
+  it('Atualiza produtos no service com id inválido', async function () {
+    sinon.stub(productsModel, 'updateProductModel').resolves({ status: 404, data: { message: 'Product not found' } });
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.updateProductService({ status: 404, data: { message: 'Product not found' } });
+
+    expect(res.status.calledWith(404));
+    expect(res.json.calledWith({ status: 404, data: { message: 'Product not found' } }));
+  });
+
+  it('Deleta um produto do service', async function () {
+    sinon.stub(productsModel, 'deleteProductModel').resolves({ status: 204 });
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.deleteProductService({ status: 204 });
+
+    expect(res.status.calledWith(204));
+  });
+
+  it('Deleta um produto do service com id inválido', async function () {
+    const error = { status: 404, data: { message: 'Product not found' } };
+    sinon.stub(productsModel, 'deleteProductModel').resolves(error);
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.deleteProductService(error);
+
+    expect(res.status.calledWith(404));
+    expect(res.json.calledWith(error));
   });
 
   afterEach(function () {
